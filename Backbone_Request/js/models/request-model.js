6dd9fd94ac6c148245b6f6b0c352926ff9e6c3f1
Duc Hoang <ducahoang@gmail.com>
2013-11-06 18:10:29 -0500

    new stuff

--- Backbone_Request/js/models/request-model.js
+++ Backbone_Request/js/models/request-model.js
@@ -1,9 +1,10 @@
-var session = session || {};
 (function(){
 	request = Backbone.Model.extend({
 		defaults:{
 			id:'',
 			title:'',
+			requestorId:'',
+			beneficiaryId:'',
 			status:'Pending',
 			selected:false
 		},
@@ -12,7 +13,26 @@ var session = session || {};
 			this.save({
 				selected : !this.get("selected")
 			});
+		},
+		validate: function(attrs){
+			var errors = this.errors = {};
+			if (!attrs.id || attrs.id == ''){
+				errors.missingId = "Request ID is missing";
+			}
+			if (!attrs.title || attrs.title == ''){
+				errors.missingTitle = "Title is missing";
+			}
+			if (!attrs.requestorId || attrs.requestorId == ''){
+				errors.missingRequestorId = "Requestor ID is missing";
+			}
+			if (!attrs.beneficiaryId || attrs.beneficiaryId == ''){
+				errors.missingBeneficiaryId = "Beneficiaty ID is missing";
+			}
+			if (!_.isEmpty(errors))
+				return errors;
+		},
+		url:function(){
+			return "request/"+this.get("id")+"/store";
 		}
 	});	
-	//session.requestItem = new request();
 })();
--- Backbone_Request/js/test/test.js
+++ Backbone_Request/js/test/test.js
@@ -1,5 +1,39 @@
 var server= {};
-describe("Test user model object",function(){
+
+describe("Test request model object", function(){
+	it("Should create an request object with default values", function(){
+		var testRequest = new request();
+		expect(testRequest).toBeDefined();
+		expect(testRequest.get("status")).toEqual("Pending");
+	});
+	it("Should trigger save method when toggle the selected state", function(){
+		var testRequest = new request();
+		spyOn(testRequest,"save");
+		testRequest.toggle();
+		expect(testRequest.save).toHaveBeenCalledWith({selected:!testRequest.get("selected")});
+		testRequest.save.reset();
+	});
+	it("Should call validate method before calling save", function(){
+		var testRequest = new request();
+		spyOn(testRequest,"validate");
+		spyOn(testRequest,"save");
+		testRequest.toggle();
+		expect(testRequest.validate).toHaveBeenCalled();
+		expect(testRequest.save).toHaveBeenCalled();
+		testRequest.validate.reset();
+		testRequest.save.reset();
+	});
+	it("Should raise an error when saving the request with empty id", function(){
+		var testRequest = new request();
+		testRequest.toggle();
+		spyOn(testRequest,"validate").andCallThrough();
+		testRequest.toggle();
+		expect(testRequest.validate).toHaveBeenCalled();
+		testRequest.validate.reset();
+	});
+});
+
+/*describe("Test user model object",function(){
 	describe("Test user login", function(){
 		beforeEach(function(){
 			server = sinon.fakeServer.create();
@@ -9,8 +43,8 @@ describe("Test user model object",function(){
 		});
 		it("Should call the request view user logged in event handler", function(){
 			currentUser = new user();
-			/*var requestView = new session.RequestView();
-			var spy = sinon.spy(requestView.userLoggedInEventHandler);*/
+			var requestView = new session.RequestView();
+			var spy = sinon.spy(requestView.userLoggedInEventHandler);
 			currentUser.fetch();
 		    server.respond();
 		    expect(currentUser.get("authenticated")).toEqual(true);
@@ -39,3 +73,4 @@ describe("Test request collection object", function(){
 		});
 	});
 });
+*/
